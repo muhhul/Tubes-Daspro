62d9e66739777a99ebeb474627803fee1b948b32
muhhul <fatihulkuliah@gmail.com>
2023-04-10 14:24:33 +0700

    update fungsi load

---
+++ function.py
@@ -0,0 +1,15 @@
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Masukkan Nama Folder')
+parser.add_argument('namaFolder', type=str, nargs = '?', help='Nama Folder', default='')
+args = parser.parse_args()
+
+if args.namaFolder=='Tubes':
+    print("Loading")
+elif args.namaFolder=='':
+    print("Tidak ada nama folder yang diberikan")
+    sys.exit()
+else:
+    print("Nama Folder salah")
+    sys.exit()
